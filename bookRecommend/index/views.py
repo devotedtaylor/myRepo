068bd478a64b218a525c0b9f9b5c37453aa92f5c
devotedtaylor <136366574@qq.com>
2019-06-26 14:33:22 +0800

    up1.6

--- bookRecommend/index/views.py
+++ bookRecommend/index/views.py
@@ -6,34 +6,34 @@ from login.views import connect,close
 # Create your views here.
 
 #猜你喜欢模块
-def index(request,name):
+def index(request,stu_id,grade):
     #连接数据库
     db,cursor = connect()
     #转换成gbk编码
     #namegbk = name.encode("gbk")
-    namegbk=name
+    #namegbk=name
     #获取当前用户的id
-    sql_getid = "select stu_id from user where username='"+namegbk+"'"
-    cursor.execute(sql_getid)
-    for row in cursor.fetchall():
-        uid = row[0]
+    #sql_getid = "select stu_id from user where username='"+stu_id+"' and "
+    #cursor.execute(sql_getid)
+    #for row in cursor.fetchall():
+     #   uid = row[0]
     # print uid
     #获取推荐结果
     #bookid_list = adjustrecommend(uid)
-
     #
-    sql_getreco= "select title from recommend where username='"+namegbk+"'"
-    bookid_list=[]
+    sql_getreco= "select item_ids from rank where stu_id='"+stu_id+"' and stu_grade='"+grade+"'"
+    #bookid_list=[]
     cursor.execute(sql_getreco)
     for row in cursor.fetchall():
-        bookid_list.append(row[0])
+        #bookid_list.append(row[0])
+        bookid_list=row[0]
     #
 
     # print bookid_list
     bookdic = {}
     #定义sql，提交并查询
     for bid in bookid_list:
-        sql = "select * from book where title = '"+bid+"'"
+        sql = "select * from book where item_id = '"+bid+"'"
         #sql = "select * from recommend where username='" + namegbk + "'"
         cursor.execute(sql)
         for row in cursor.fetchall():
@@ -43,12 +43,13 @@ def index(request,name):
     #返回语句，带回相应的数据
     return  render_to_response("index.html",{
         "bookdic":bookdic,
-        "name":name,
+        "stu_id":stu_id,
+        "grade": grade,
         "color1": "red",
         "flag":True,
     })
 
-def hot(request,name):
+def hot(request,stu_id,grade):
     booklist = []
     #连接数据库
     db,cursor = connect()
@@ -70,7 +71,9 @@ def hot(request,name):
     #返回语句，带回相应的数据
     return  render_to_response("index.html", {
         "booklist":newbooklist,
-        "name":name,
+        #"name":name,
+        "stu_id":stu_id,
+        "grade": grade,
         "color2": "red",
         "flag":False,
     })
